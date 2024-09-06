a021e52e0e01210ac1bc5914cdffde2c22e215b1
samudiperera09 <samudiperera125@gmail.com>
2024-09-06 22:58:40 +0530

    New components added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
 import Navbar from "@/components/navbar";
 import Image from "next/image";
 import Login from "@/components/login";
+import LoginPage from "@/components/login";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar />
       <Image
         src="/assets/images/background.png"
         alt="hero"
         className="w-full h-screen object-cover"
         width={1920}
         height={600}
-      />
+      /> */}
       {/* <Login/> */}
+      <LoginPage/>
     </>
   );
 }
